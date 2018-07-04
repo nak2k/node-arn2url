d39ba3cce6d0f3f8a0d20782fca0320f417c442a
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2018-07-04 12:33:55 +0900

    Support some IAM ARNs

--- test/test.js
+++ test/test.js
@@ -1,6 +1,25 @@
 const test = require('tape');
 const { arn2url } = require('..');
 
+test('test iam', t => {
+  t.plan(5);
+
+  t.deepEqual(arn2url('arn:aws:iam::123456789012:root'),
+    [null, 'https://console.aws.amazon.com/iam/home']);
+
+  t.deepEqual(arn2url('arn:aws:iam::123456789012:user/Bob'),
+    [null, 'https://console.aws.amazon.com/iam/home#/users/Bob']);
+
+  t.deepEqual(arn2url('arn:aws:iam::123456789012:group/Developers'),
+    [null, 'https://console.aws.amazon.com/iam/home#/groups/Developers']);
+
+  t.deepEqual(arn2url('arn:aws:iam::123456789012:role/S3Access'),
+    [null, 'https://console.aws.amazon.com/iam/home#/roles/S3Access']);
+
+  t.deepEqual(arn2url('arn:aws:iam::123456789012:policy/UsersManageOwnCredentials'),
+    [null, 'https://console.aws.amazon.com/iam/home#/policies/arn:aws:iam::123456789012:policy/UsersManageOwnCredentials']);
+});
+
 test('test s3', t => {
   t.plan(2);
 
